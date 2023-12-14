36f8977aed73b11882e46753f47afd6caed7a748
André Couto <andreww.max@hotmail.com>
2023-12-14 00:40:18 -0300

    fix: error on build maps nextjs routes, by exclude .next

--- Miscelanious/next-geojson/next.config.js
+++ Miscelanious/next-geojson/next.config.js
@@ -2,3 +2,6 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+// const withTranspile = require("next-transpile-modules")(["ol", "rlayers"]);
+// module.exports = withTranspile({ experimental: { esmExternals: "loose" } });
--- Miscelanious/next-geojson/src/app/page.tsx
+++ Miscelanious/next-geojson/src/app/page.tsx
@@ -1,16 +1,32 @@
 "use client";
+
 import "ol/ol.css";
 
 import React, { useCallback } from "react";
 import { fromLonLat } from "ol/proj";
 import GeoJSON from "ol/format/GeoJSON";
-import { RMap, ROSM, RLayerVector, RStyle, RControl, RLayerTile } from "rlayers";
+// import { RMap, ROSM, RLayerVector, RStyle, RControl, RLayerTile } from "rlayers";
+import { RStyle, RControl } from "rlayers";
 
-import geojsonFeatures from "../../data/southamericamemes.json";
 import { Feature } from "ol";
 import { LineString, Point } from "ol/geom";
 
+import geojsonFeatures from "../../data/southamericamemes.json";
+
+import dynamic from "next/dynamic";
+
+const RMap = dynamic(() => import("rlayers").then((module) => module.RMap), { ssr: false });
+const ROSM = dynamic(() => import("rlayers").then((module) => module.ROSM), { ssr: false });
+const RLayerVector = dynamic(() => import("rlayers").then((module) => module.RLayerVector), { ssr: false });
+// const RStyle = dynamic(() => import("rlayers").then((module) => module.RStyle), { ssr: false });
+// const RControl = dynamic(() => import("rlayers").then((module) => module.RControl), { ssr: false });
+const RLayerTile = dynamic(() => import("rlayers").then((module) => module.RLayerTile), { ssr: false });
+// const DynamicHeader = dynamic(() => import "ol/ol.css", {
+// 	ssr: false,
+// });
+
 const center = fromLonLat([-40.12736, -19.500781]);
+
 export default function Simple(): JSX.Element {
 	// basic map
 	// return (
@@ -18,21 +34,21 @@ export default function Simple(): JSX.Element {
 	// 		<ROSM />
 	// 	</RMap>
 	// );
-	console.log(geojsonFeatures);
-	const layersButton = <button>&#9776;</button>;
+	// console.log(geojsonFeatures);
+	const layersButton = <button className="layersButton">&#9776;</button>;
 
 	return (
 		<div>
 			<RMap width={"100%"} height={"100dvh"} initial={{ center: center, zoom: 11 }}>
 				{/* <ROSM /> */}
-				<RControl.RLayers element={layersButton}>
-					<ROSM properties={{ label: "OpenStreetMap" }} />
-					<RLayerTile
-						properties={{ label: "OpenTopo" }}
-						url="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-						attributions="Google"
-					/>
-				</RControl.RLayers>
+				{/* <RControl.RLayers element={layersButton}> */}
+				{/* <ROSM properties={{ label: "OpenStreetMap" }} /> */}
+				<RLayerTile
+					properties={{ label: "Google Maps satellite" }}
+					url="https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+					attributions="Google"
+				/>
+				{/* </RControl.RLayers> */}
 				<RLayerVector
 					zIndex={10}
 					/* Input data will have to be typed too */
